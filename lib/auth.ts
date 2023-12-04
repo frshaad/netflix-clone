d176ee1086de844a9117a73309ac1d7fa6869dcf
Farshad Hatami <farshad.hatami95@gmail.com>
2023-12-04 23:39:30 +0330

    add basic next auth routes

---
+++ lib/auth.ts
@@ -0,0 +1,7 @@
+import type { AuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions: AuthOptions = {
+  providers: [],
+};

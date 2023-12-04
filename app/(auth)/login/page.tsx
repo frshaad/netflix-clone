68d6691635692ef57394d7917506c78b5aa8fc18
Farshad Hatami <farshad.hatami95@gmail.com>
2023-12-04 23:12:07 +0330

    auth components ui done

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,3 +1,54 @@
+import Link from "next/link";
+import { FaGithub, FaGoogle } from "react-icons/fa6";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+
 export default function LoginPage() {
-  return <div>LoginPage</div>;
+  return (
+    <form action="">
+      <Card className="mt-24 min-w-[360px] bg-black/80 md:mt-0">
+        <CardHeader>
+          <CardTitle>Login</CardTitle>
+          <CardDescription>Log in to an existing account</CardDescription>
+        </CardHeader>
+
+        <CardContent className="flex flex-col gap-4">
+          <Input type="email" name="email" placeholder="Email" className="" />
+          <Button type="submit" variant="destructive" className="bg-[#e50914]">
+            Log In
+          </Button>
+
+          <Separator />
+
+          <div className="flex items-center justify-center gap-x-3">
+            <Button size="icon" variant="outline">
+              <FaGithub size={20} />
+            </Button>
+            <Button size="icon" variant="outline">
+              <FaGoogle size={20} />
+            </Button>
+          </div>
+        </CardContent>
+
+        <CardFooter>
+          <div className="flex items-center gap-1 text-sm text-gray-500">
+            <p>New to Netflix?</p>
+            <Link href="/sign-up" className="text-white hover:underline">
+              Sign Up
+            </Link>
+          </div>
+        </CardFooter>
+      </Card>
+    </form>
+  );
 }
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,3 +1,54 @@
+import Link from "next/link";
+import { FaGithub, FaGoogle } from "react-icons/fa6";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+
 export default function SignUpPage() {
-  return <div>SignUpPage</div>;
+  return (
+    <form action="">
+      <Card className="mt-24 min-w-[360px] bg-black/80 md:mt-0">
+        <CardHeader>
+          <CardTitle>Sign Up</CardTitle>
+          <CardDescription>Create an account</CardDescription>
+        </CardHeader>
+
+        <CardContent className="flex flex-col gap-4">
+          <Input type="email" name="email" placeholder="Email" className="" />
+          <Button type="submit" variant="destructive" className="bg-[#e50914]">
+            Sign Up
+          </Button>
+
+          <Separator />
+
+          <div className="flex items-center justify-center gap-x-3">
+            <Button size="icon" variant="outline">
+              <FaGithub size={20} />
+            </Button>
+            <Button size="icon" variant="outline">
+              <FaGoogle size={20} />
+            </Button>
+          </div>
+        </CardContent>
+
+        <CardFooter>
+          <div className="flex items-center gap-1 text-sm text-gray-500">
+            <p>Already have an account?</p>
+            <Link href="/login" className="text-white hover:underline">
+              Log in
+            </Link>
+          </div>
+        </CardFooter>
+      </Card>
+    </form>
+  );
 }

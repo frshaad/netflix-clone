8c3c978c8b20d748be9203a98914bbd8a81d5966
Farshad Hatami <farshad.hatami95@gmail.com>
2023-12-13 12:58:46 +0330

    add user's wathclist page

---
+++ app/home/user/list/fetchUserWatchlist.helper.ts
@@ -0,0 +1,26 @@
+import prisma from "@/lib/db";
+
+export const fetchUserWatchlist = async (userId: string) => {
+  const data = await prisma.watchlist.findMany({
+    where: {
+      userId,
+    },
+    select: {
+      Movie: {
+        select: {
+          title: true,
+          age: true,
+          duration: true,
+          imageString: true,
+          overview: true,
+          release: true,
+          id: true,
+          watchlists: true,
+          youtubeString: true,
+        },
+      },
+    },
+  });
+
+  return data;
+};

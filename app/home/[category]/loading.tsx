6d7bdfd0aff351eac69a5f727e1619c90d3d507a
Farshad Hatami <farshad.hatami95@gmail.com>
2023-12-16 12:43:12 +0330

    add loading ui to pages

---
+++ app/home/[category]/loading.tsx
@@ -0,0 +1,13 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+export default function Loading() {
+  return (
+    <section className="mt-10 grid grid-cols-1 gap-6 px-5 sm:grid-cols-2 sm:px-0 md:grid-cols-3 lg:grid-cols-4">
+      <Skeleton className="h-60 w-72" />
+      <Skeleton className="h-60 w-72" />
+      <Skeleton className="h-60 w-72" />
+      <Skeleton className="h-60 w-72" />
+      <Skeleton className="h-60 w-72" />
+    </section>
+  );
+}

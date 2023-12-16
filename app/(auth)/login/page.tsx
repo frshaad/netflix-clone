bc58ef1dc5469893a6b64eb2c915341c51e63eac
Farshad Hatami <farshad.hatami95@gmail.com>
2023-12-16 13:22:38 +0330

    refactor login page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -16,22 +16,19 @@ export default async function LoginPage() {
   const session = await getServerSession(authOptions);
 
   if (session) {
-    return redirect("/home");
+    redirect("/home");
   }
 
   return (
-    <>
-      <Card className="mt-24 min-w-[360px] bg-black/80 md:mt-0">
-        <CardHeader>
-          <CardTitle>Sign In</CardTitle>
-          <CardDescription>Continue to your Netflix account</CardDescription>
-        </CardHeader>
-
-        <CardContent className="flex flex-col gap-4">
-          <AuthButton provider="google" />
-          <AuthButton provider="github" />
-        </CardContent>
-      </Card>
-    </>
+    <Card className="mt-24 min-w-[360px] bg-black/80 md:mt-0">
+      <CardHeader>
+        <CardTitle>Sign In</CardTitle>
+        <CardDescription>Continue to your Netflix account</CardDescription>
+      </CardHeader>
+      <CardContent className="flex flex-col gap-4">
+        <AuthButton provider="google" />
+        <AuthButton provider="github" />
+      </CardContent>
+    </Card>
   );
 }

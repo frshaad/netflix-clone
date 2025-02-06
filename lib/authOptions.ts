66025081412bdbcaa0ab1bac48abbc7bea7e005f
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2025-02-06 14:15:13 +0330

    refactor: use environment variables for authentication provider credentials

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -1,20 +1,22 @@
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import type { NextAuthOptions } from "next-auth";
-import GithubProvider from "next-auth/providers/github";
-import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import type { NextAuthOptions } from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
 
-import prisma from "@/lib/db";
+import prisma from '@/lib/db';
+
+import environment from './env';
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
+      clientId: environment.GITHUB_ID,
+      clientSecret: environment.GITHUB_SECRET,
     }),
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      clientId: environment.GOOGLE_CLIENT_ID,
+      clientSecret: environment.GOOGLE_CLIENT_SECRET,
     }),
   ],
 };

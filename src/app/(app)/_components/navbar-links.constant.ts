c27bdd1c159994b2c4e82c67485c2fff646ac8ff
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2025-02-07 01:02:08 +0330

    chore: update Node.js engine version in package.json; modify navbar links for cleaner routing

--- src/app/(app)/_components/navbar-links.constant.ts
+++ src/app/(app)/_components/navbar-links.constant.ts
@@ -5,9 +5,9 @@ type linkProperties = {
 };
 
 export const links: linkProperties[] = [
-  { id: '1', name: 'Home', href: '/home' },
-  { id: '2', name: 'Tv Shows', href: '/home/shows' },
-  { id: '3', name: 'Movies', href: '/home/movies' },
-  { id: '4', name: 'Recently Added', href: '/home/recently' },
-  { id: '5', name: 'My List', href: '/home/user/list' },
+  { id: '1', name: 'Home', href: '/' },
+  { id: '2', name: 'TV Shows', href: '/shows' },
+  { id: '3', name: 'Movies', href: '/movies' },
+  { id: '4', name: 'Recently Added', href: '/recent' },
+  { id: '5', name: 'My List', href: '/list' },
 ];
